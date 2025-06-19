024581d24b5a267a2cc2b0c06be36ed203eca803
Muhammad Ahmar Ali <159634677+Ahmar06@users.noreply.github.com>
2025-06-19 16:09:10 +0500

    Update main.py

--- main.py
+++ main.py
@@ -42,15 +42,16 @@ async def on_chat_start():
 @cl.on_message
 async def handle_message(message:cl.Message):
     history = cl.user_session.get("history", [])
+    msg = cl.Message(content="")
+    await msg.send()
     history.append({"role":"user","content":message.content})
-    result = await Runner.run(
+    result = Runner.run_streamed(
         agent,
         input = history,
         run_config = config
     )
+    async for event in result.stream_events():
+        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+            await msg.stream_token(event.data.delta)
     history.append({"role":"assistant","content":result.final_output})
-    print(history)
     cl.user_session.set("history", history)
-    await cl.Message(content=result.final_output).send()
-
-
