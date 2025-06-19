5a2e72bc3498030d41141ba46c98e5e5b95f8ba9
Muhammad Ahmar Ali <159634677+Ahmar06@users.noreply.github.com>
2025-06-19 15:44:50 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,56 @@
+from agents import Agent, OpenAIChatCompletionsModel,AsyncOpenAI,Runner,RunConfig
+from dotenv import load_dotenv
+import os
+import asyncio
+from openai.types.responses import ResponseTextDeltaEvent
+import chainlit as cl
+
+
+
+load_dotenv()
+
+MODEL_NAME = "gemini-2.0-flash"
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+external_client = AsyncOpenAI(
+    api_key = GEMINI_API_KEY,
+    base_url = "https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model = MODEL_NAME,
+    openai_client = external_client,    
+)
+
+config = RunConfig(
+    model = model,
+    model_provider = external_client,
+    tracing_disabled = True,
+)
+
+agent = Agent(
+    name = "Assistant",
+    instructions ="You are a coading assistant",
+    model = model,
+)
+
+@cl.on_chat_start
+async def on_chat_start():
+    cl.user_session.set("history",[])
+    await cl.Message(content="Hello, I am your coding assistant. How can I help you today?").send() 
+
+@cl.on_message
+async def handle_message(message:cl.Message):
+    history = cl.user_session.get("history", [])
+    history.append({"role":"user","content":message.content})
+    result = await Runner.run(
+        agent,
+        input = history,
+        run_config = config
+    )
+    history.append({"role":"assistant","content":result.final_output})
+    print(history)
+    cl.user_session.set("history", history)
+    await cl.Message(content=result.final_output).send()
+
+
