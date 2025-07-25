d3eb7cf5d44b3be29d98508ef8c60c2003e69da5
Muhammad Ahmar Ali <159634677+Ahmar06@users.noreply.github.com>
2025-07-25 14:59:44 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,57 +1,42 @@
-from agents import Agent, OpenAIChatCompletionsModel,AsyncOpenAI,Runner,RunConfig
+from agents import Agent,OpenAIChatCompletionsModel,Runner,RunConfig,AsyncOpenAI
+import os 
 from dotenv import load_dotenv
-import os
 import asyncio
-from openai.types.responses import ResponseTextDeltaEvent
-import chainlit as cl
-
-
 
 load_dotenv()
 
-MODEL_NAME = "gemini-2.0-flash"
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+while True:
+    async def main():
 
-external_client = AsyncOpenAI(
-    api_key = GEMINI_API_KEY,
-    base_url = "https://generativelanguage.googleapis.com/v1beta/openai/",
-)
+        MODEL_NAME = "gemini-2.0-flash"
+        GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
-model = OpenAIChatCompletionsModel(
-    model = MODEL_NAME,
-    openai_client = external_client,    
-)
+        external_client = AsyncOpenAI(
+            api_key = GEMINI_API_KEY,
+            base_url = "https://generativelanguage.googleapis.com/v1beta/openai/",
+        )
+        model = OpenAIChatCompletionsModel(
+            model = MODEL_NAME,
+            openai_client = external_client,
+        )
 
-config = RunConfig(
-    model = model,
-    model_provider = external_client,
-    tracing_disabled = True,
-)
+        config = RunConfig(
+          tracing_disabled = True,
+          model = model,
+          model_provider = external_client,
+        ) 
 
-agent = Agent(
-    name = "Assistant",
-    instructions ="You are a coading assistant",
-    model = model,
-)
+        agent = Agent(
+            name = "Assistant",
+            instructions = "You are a helpful assistant Answer academic questions Provide study tips Summarize small text passages",
+            model = model,
+        )
+        result = await Runner.run(
+            agent,
+            input = input("Enter your query: "),
+            run_config = config,
+        )
 
-@cl.on_chat_start
-async def on_chat_start():
-    cl.user_session.set("history",[])
-    await cl.Message(content="Hello, I am your coding assistant. How can I help you today?").send() 
+        print(result.final_output)
 
-@cl.on_message
-async def handle_message(message:cl.Message):
-    history = cl.user_session.get("history", [])
-    msg = cl.Message(content="")
-    await msg.send()
-    history.append({"role":"user","content":message.content})
-    result = Runner.run_streamed(
-        agent,
-        input = history,
-        run_config = config
-    )
-    async for event in result.stream_events():
-        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
-            await msg.stream_token(event.data.delta)
-    history.append({"role":"assistant","content":result.final_output})
-    cl.user_session.set("history", history)
+    asyncio.run(main())
